0e5126165a4bc3dc220c63a91585aa1cbd16ddb3
ThiagoRech1997 <thiagorech.1997@gmail.com>
2019-06-30 10:54:21 -0300

    android - update

--- app/src/pages/Servicos/index.js
+++ app/src/pages/Servicos/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import api from './../../services/api';
 
 import { 
   View,
@@ -9,10 +10,41 @@ import {
 import styles from './styles';
 
 export default class Servicos extends Component {
+  state = {
+    servicos: [],
+  };
+
+  renderItem = ({item}) => (
+    <View>
+      <Text>{item.descricao}</Text>
+      <Text>{item.itens.descricao}</Text>
+      <Text>{item.itens.quantidade}</Text>
+      <Text>{item.itens.valor}</Text>
+      <Text>{item.cliente.nome}</Text>
+      <Text>{item.funcionario.nome}</Text>
+      <Text>{item.valor}</Text>
+      <Text>{item.status}</Text>
+    </View>
+  );
+
+  loadServicos = async () => {
+    const response = await api.get('/servicos');
+    this.setState({ servicos: response.data });
+  };
+
+  componentDidMount() {
+    this.loadServicos();
+  };
+
   render() {
     return (
       <View>
-        <FlatList />
+        <FlatList 
+          style={styles.lista}
+          data={this.state.servicos}
+          keyExtractor={item => item._id}
+          renderItem={this.renderItem}
+        />
       </View>
     );
   }
